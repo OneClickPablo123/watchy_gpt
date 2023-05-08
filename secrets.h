8986afeea524047c07713391d1d45a83fe359bd2
OneClickPablo <92474595+OneClickPablo123@users.noreply.github.com>
2023-05-08 15:31:26 +0200

    Update secrets.h

--- secrets.h
+++ secrets.h
@@ -5,12 +5,19 @@
 const char* WIFI_SSID = "your_wifi_ssid";
 const char* WIFI_PASSWORD = "your_wifi_password";
 
-// Google Speech to Text API credentials
-const char* GOOGLE_API_KEY = "your_google_api_key";
+// Google API credentials
+const char* GOOGLE_ACCESS_TOKEN = "your_google_access_token";
+const char* GOOGLE_PRIVATE_KEY = "-----BEGIN PRIVATE KEY-----\n"
+"your_google_private_key_here\n"
+"-----END PRIVATE KEY-----\n";
+const char* GOOGLE_CLIENT_CERT = "-----BEGIN CERTIFICATE-----\n"
+"your_google_client_cert_here\n"
+"-----END CERTIFICATE-----\n";
+const char* GOOGLE_CERTIFICATE = "-----BEGIN CERTIFICATE-----\n"
+"your_google_certificate_here\n"
+"-----END CERTIFICATE-----\n";
 const char* GOOGLE_SPEECH_TO_TEXT_API_HOST = "speech.googleapis.com";
-const int GOOGLE_SPEECH_TO_TEXT_API_PORT = 443;
 const char* GOOGLE_SPEECH_TO_TEXT_API_ENDPOINT = "/v1/speech:recognize";
-const char* GOOGLE_SPEECH_TO_TEXT_API_FINGERPRINT = "AB:CD:EF:...";
 
 // ChatGPT API credentials
 const char* CHATGPT_API_KEY = "your_chatgpt_api_key";
