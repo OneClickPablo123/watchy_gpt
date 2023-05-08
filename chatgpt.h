312de704aabc04944117acd8d5134fef5887e82d
OneClickPablo <92474595+OneClickPablo123@users.noreply.github.com>
2023-05-08 15:15:25 +0200

    Update chatgpt.h

--- chatgpt.h
+++ chatgpt.h
@@ -1,52 +1,43 @@
-#ifndef CHATGPT
-#define CHATGPT
 
-#include <WiFiClientSecure.h>
+#ifndef CHATGPT_H
+#define CHATGPT_H
+
 #include <HTTPClient.h>
 #include <ArduinoJson.h>
-
 #include "secrets.h"
+#include "speechtotext.h"
 
-String chatGPT(String prompt) {
-  WiFiClientSecure client;
-  client.setCACert(root_ca);
+const String GPT_API_ENDPOINT = "https://api.openai.com/v1/engines/" + String(OPENAI_API_ENGINE) + "/completions";
 
-  HTTPClient http;
-  http.begin(client, "https://" + GPT_HOST + GPT_PATH);
-  http.addHeader("Content-Type", "application/json");
-  http.addHeader("Authorization", "Bearer " + GPT_TOKEN);
+String getChatResponse(String userText) {
+    String result = "";
 
-  // Build JSON request payload
-  DynamicJsonDocument payload(2048);
-  payload["prompt"] = prompt;
-  payload["temperature"] = 0.7;
-  payload["max_tokens"] = 50;
-  payload["stop"] = "\n";
+    // Build the request body for the GPT API
+    StaticJsonDocument<512> requestBody;
+    requestBody["prompt"] = speechToText();
+    requestBody["max_tokens"] = 50;
+    requestBody["temperature"] = 0.5;
 
-  String payload_str;
-  serializeJson(payload, payload_str);
-  Serial.println(payload_str);
+    String requestBodyString;
+    serializeJson(requestBody, requestBodyString);
 
-  int httpCode = http.POST(payload_str);
+    // Send the request to the GPT API
+    HTTPClient httpClient;
+    httpClient.begin(GPT_API_ENDPOINT);
+    httpClient.addHeader("Content-Type", "application/json");
+    httpClient.addHeader("Authorization", "Bearer " + String(CHATGPT_API_KEY));
+    int httpCode = httpClient.POST(requestBodyString);
 
-  if (httpCode > 0) {
-    if (httpCode == HTTP_CODE_OK) {
-      String response = http.getString();
-      Serial.println(response);
-
-      DynamicJsonDocument json_resp(4096);
-      deserializeJson(json_resp, response);
-      String choices_0_text = json_resp["choices"][0]["text"];
-      return choices_0_text;
-    } else {
-      Serial.printf("[HTTP] POST... failed, error: %s\n", http.errorToString(httpCode).c_str());
+    // Parse the response from the GPT API
+    if (httpCode > 0) {
+        String responseBody = httpClient.getString();
+        StaticJsonDocument<1024> responseJson;
+        deserializeJson(responseJson, responseBody);
+        result = responseJson["choices"][0]["text"].as<String>();
     }
-  } else {
-    Serial.printf("[HTTP] Unable to connect\n");
-  }
 
-  http.end();
-  return "";
+    httpClient.end();
+    return result;
 }
 
 #endif
